bd97a9081afd3af3dc29c71a8ff2b4deaf8d499c
Nikolai Melnikov <melnnik@yahoo.com>
2022-05-21 12:54:06 -0400

    Hooks for before and after the message is sent

--- src/services/users/users.class.js
+++ src/services/users/users.class.js
@@ -1,5 +1,31 @@
-const { Service } = require('feathers-nedb');
+const {Service} = require('feathers-nedb');
+
+const crypto = require('crypto');
+
+const gravatarUrl = 'https://s.gravatar.com/avatar';
+
+const query = 's=60';
+
+const getGravatar = email => {
+  // Gravatar uses MD5 hashes from an email address (all lowercase) to get the image
+  const hash = crypto.createHash('md5').update(email.toLowerCase()).digest('hex');
+  // Return the full avatar URL
+  return `${gravatarUrl}/${hash}?${query}`;
+};
 
 exports.Users = class Users extends Service {
-  
+  create(data, params) {
+    const {email, password, githubId} = data;
+
+    const avatar = data.avatar || getGravatar(email);
+
+    const userData = {
+      email,
+      password,
+      githubId,
+      avatar
+    };
+
+    return super.create(userData, params);
+  }
 };
